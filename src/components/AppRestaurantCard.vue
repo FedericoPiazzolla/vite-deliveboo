3723614002c13ada8c6729865bc6ca4e50c7a6e2
giuliogallo94 <giuliogallo1994@gmail.com>
2024-02-26 11:15:45 +0100

    add: component for singleRestaurant card

--- src/components/AppRestaurantCard.vue
+++ src/components/AppRestaurantCard.vue
@@ -1,45 +1,29 @@
 <script>
 import axios from "axios";
-import AppRestaurantCard from "./AppRestaurantCard.vue";
 export default {
   data() {
     return {
       restaurants: [],
     };
   },
-  components: {
-    AppRestaurantCard,
-  },
-
-  created() {
-    axios.get(`http://127.0.0.1:8000/api/restaurants`).then((resp) => {
-      console.log(resp.data.results);
-      this.restaurants = resp.data.results;
-    });
-  },
-  methods: {
-    // searchRestaurant() {
-    //   // 1. svuotare array restaurants[]
-    //   // 2. chiamata axios
-    //   // 3. pushare nell'array restaurants la nuova resp
-    // },
+  props: {
+    singleRestaurant: Object,
   },
 };
 </script>
 
 <template>
-  <section class="ms_restaurants">
-    <h2 class="ms_title-restaurants">" I ristoranti intorno a te! "</h2>
-
-    <div class="container">
-      <div class="row g-3 ms_row">
-        <AppRestaurantCard
-          v-for="(restaurant, index) in restaurants"
-          :key="index"
-          :singleRestaurant="restaurant" />
+  <div class="ms_col col-md-4 col-sm-12">
+    <router-link :to="{ name: 'singleRestaurant' }">
+      <div
+        class="cards"
+        :style="`background-image: url(${singleRestaurant.restaurant_image})`">
+        <h5>
+          {{ singleRestaurant.restaurant_name }}
+        </h5>
       </div>
-    </div>
-  </section>
+    </router-link>
+  </div>
 </template>
 
 <style lang="scss" scoped>
