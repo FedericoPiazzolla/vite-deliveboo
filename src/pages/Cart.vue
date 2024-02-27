03df7c7621158be6b8f49c9004727ca711f289c3
FedericoPiazzolla <Federicop257@gmail.com>
2024-02-27 18:21:36 +0100

    style: cart

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -25,7 +25,7 @@ export default {
         saveCart(store.updatedCart);
       }
     },
-    removeDish() {
+    removeDishes() {
       localStorage.clear();
       store.updatedCart = [];
     },
@@ -47,21 +47,21 @@ export default {
 <template>
   <section class="ms_cart-section">
     <!-- carrello con lista ordinata dei prodotti, totale da pagare e bottoni -->
-    <section id="cart-container" v-if="store.updatedCart.length > 0">
+    <section id="cart-container" class="p-3 py-5" v-if="store.updatedCart.length > 0">
       
-        <h2 class="mb-4 text-uppercase text-center">Carrello <i class="fa-solid fa-cart-shopping"></i></h2>
+        <h2 class="pb-5 text-center m-0">Il tuo ordine <i class="fa-solid fa-cart-shopping"></i></h2>
       
-        <h4 class="mb-3">{{ store.updatedCart[0].restaurant.restaurant_name }}</h4>
+        <h4 class="ms_restaurant-title mb-3 d-inline">{{ store.updatedCart[0].restaurant.restaurant_name }}</h4>
 
-        <ol class="list-group list-group-numbered">
-          <li v-for="(product, index) in store.updatedCart" :key="index" class="ps-4 rounded-5 mb-3 flex-column flex-md-row list-group-item d-flex justify-content-between align-items-start">
+        <ol class="list-group mt-4">
+          <li v-for="(product, index) in store.updatedCart" :key="index" class="ps-4 rounded-5 mb-3 flex-row list-group-item d-flex justify-content-between align-items-center">
+
             <div class="ms-md-2 me-auto">
-              <p class="fw-bold mb-1">
-                {{ product.name }}
-                <span class="ps-md-3 d-block d-md-inline">&euro;{{ product.price }}</span>
-              </p>
+              <p class=" ms_dish-name fw-bold mb-1">{{ product.name }}</p>
+              <span class="ms_price d-inline-block">&euro; {{ product.price }}</span>
             </div>
-            <div class="d-inline-block mt-2 mb-2 mb-md-0">
+
+            <div class="ms_quantity-show d-inline-block mt-2 mb-2 mb-md-0">
               <button class="px-2 py-1" @click="removeQuantity(index)">
                 <i class="fa-solid fa-minus"></i>
               </button>
@@ -73,9 +73,9 @@ export default {
           </li>
         </ol>
 
-        <div class="d-flex justify-content-end">
-          <p class="text-decoration-underline mb-3 text-warning px-5 fw-bolder fs-3">Totale: &euro; {{ calcTotal() }}</p>
-          <button @click="removeDish()">Elimna piatti</button>
+        <div class="ms_total-price d-flex justify-content-between justify-content-md-end">
+          <p>Totale: &euro; {{ calcTotal() }}</p>
+          <button class="btn btn-success">Conferma ordine</button>
         </div>
     </section>
 
@@ -86,8 +86,59 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use "../scss/partials/variables" as *;
 
 .ms_cart-section {
   min-height: 1000px;
+
+  .ms_restaurant-title {
+    background-color: $bg-btn;
+    border-radius: 2rem;
+    padding: .5rem 1rem;
+    margin-left: 1rem;
+  }
+
+  .ms_price {
+    background-color: $bg-color;
+    padding: .2rem .5rem;
+    border-radius: 2rem;
+    font-weight: bolder;
+  }
+
+  ol {
+    li {
+      background-color: $bg-btn;
+
+      .ms_quantity-show {
+        background-color: $bg-color;
+        border-radius: 2rem;
+
+        button {
+          background-color: $main-text;
+          color: $bg-btn;
+          border: 0;
+          border-radius: 50%;
+
+          &:active {
+            background-color: red;
+          }
+        }
+      }
+    }
+  }
+
+  .ms_total-price {
+    text-align: center;
+    padding-top: 1rem;
+    p {
+      margin: 0;
+      background-color: $primary;
+      font-size: 1.7rem;
+      color: $bg-color;
+      padding: .5rem .7rem;
+      border-radius: 2rem;
+    }
+  }
+
 }
 </style>
