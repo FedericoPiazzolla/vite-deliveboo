ce59f996bb663442aec3b62911210a986361234b
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-02-27 17:07:38 +0100

    hotfix: axios call in restaurants list

--- src/components/AppRestaurants.vue
+++ src/components/AppRestaurants.vue
@@ -15,7 +15,7 @@ export default {
   created() {
     axios.get(`http://127.0.0.1:8000/api/restaurants`).then((resp) => {
       this.store.restaurantLoading = false;
-      this.store.restaurantsToShow = resp.data.results.data;
+      this.store.restaurantsToShow = resp.data.results;
       console.log(this.store.restaurantsToShow);
     });
 
