3bb4981fda8bcd01d94c4ec966a019815082c2f4
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-02-29 11:30:17 +0100

    add: inputs with v-model, order now includes all necessary info (dishes, quantities and order info)

--- src/pages/Checkout.vue
+++ src/pages/Checkout.vue
@@ -1,22 +1,26 @@
 <script>
 import axios from "axios";
-import { store } from "../data/store";
+import { store, loadCart } from "../data/store";
 
 export default {
     data() {
         return {
+            importedCart: null,
             store,
             token: '',
             instance: null,
             paymentNonce: null,
             dataToSend: {
-                interested_user_name: 'Mario',
-                interested_user_surname: 'Petri',
+                interested_user_name: 'Pippo',
+                interested_user_surname: 'Pippi',
                 interested_user_address: 'Via del citofono, 19, 21021 Milano',
-                interested_user_email: 'mario.petri@gmail.com',
+                interested_user_email: 'pippo.pippi@gmail.com',
                 interested_user_phone: '+393574681755',
-                total: 10,
-                payment_method_nonce: null
+                total: 0,
+                payment_method_nonce: null,
+                // Informazioni del/dei piatto/i con la/le quantità
+                dishes_id: null,
+                dishes_quantities: null,
             }
         }
     },
@@ -35,13 +39,18 @@ export default {
 
                         dataToSend.payment_method_nonce = this.paymentNonce;
 
-                        console.log("Paymentnonce", this.paymentNonce);
+                        console.log("PaymentNonce", this.paymentNonce);
 
                         console.log("Oggetto dei dati da passare", dataToSend);
 
+                        // Una volta ottenuto il token, faccio la chiamata post per verificare il pagamento ed eventualmente salvare l'ordine
                         axios
-                        .post("http://127.0.0.1:8000/api/order", dataToSend)
-                        .then((resp) => console.log(resp))
+                            .post("http://127.0.0.1:8000/api/order", dataToSend)
+                            .then((resp) => console.log(resp))
+                            .finally(
+                                localStorage.clear('updatedCart'),
+                                console.log("Log del carrello una volta svuotato", loadCart)
+                            )
                     });
                     console.log("Pagamento avvenuto!");
                 });
@@ -49,33 +58,58 @@ export default {
         },
         generatePaymentForm() {
             axios
-            .get(`${this.store.apiUrl}api/braintree/token`)
-            .then((resp) => {
-                this.token = resp.data.clientToken;
-                console.log("Token ottenuto dalla chiamata a Braintree", this.token);
-            })
-            .finally(
-                this.brainTreeInit
-            )
+                .get(`${this.store.apiUrl}api/braintree/token`)
+                .then((resp) => {
+                    this.token = resp.data.clientToken;
+                    console.log("Token ottenuto dalla chiamata a Braintree", this.token);
+                })
+                .finally(
+                    this.brainTreeInit
+                )
         },
-        sendPaymentPayload() {
-            axios.post(`${this.store.apiUrl}api/orders`, {
-                total: 10,
-                payment_method_nonce: this.paymentNonce // Invia il nonce di pagamento al server
-            })
-                .then((resp) => console.log(resp))
+        // sendPaymentPayload() {
+        //     axios.post(`${this.store.apiUrl}api/orders`, {
+        //         total: 10,
+        //         payment_method_nonce: this.paymentNonce // Invia il nonce di pagamento al server
+        //     })
+        //         .then((resp) => console.log(resp))
+        // },
+        importCart() {
+            this.importedCart = this.store.updatedCart;
+            console.log("Carrello importato", this.importedCart);
+            let dishesId = [];
+            let dishesQuantities = [];
+            let total = 0;
+
+            this.importedCart.forEach(dish => {
+                // Pusho nei corrispettivi array gli id dei piatti e le loro quantità
+                dishesId.push(dish.id);
+                dishesQuantities.push(dish.quantity);
+
+                // Li rendo stringhe
+                dishesId.toString();
+                dishesQuantities.toString();
+
+                // Calcolo progressivamente il totale
+                total += dish.price * dish.quantity;
+            });
+
+            // Rendo gli id e le quantità delle singole stringhe
+            dishesId = dishesId.join(',');
+            dishesQuantities = dishesQuantities.join(',');
+
+            // Assegno alle chiavi nell'oggetto dei dati da inviare il risultato
+            this.dataToSend.dishes_id = dishesId;
+            this.dataToSend.dishes_quantities = dishesQuantities;
+            this.dataToSend.total = total;
+
+            console.log("id dei piatti dal carrello", this.dataToSend.dishes_id);
+            console.log("quantità dei piatti del carrello", this.dataToSend.dishes_quantities);
+            console.log("totale da pagare", this.dataToSend.total);
         }
     },
     created() {
-        // axios
-        //     .get(`${this.store.apiUrl}api/braintree/token`)
-        //     .then((resp) => {
-        //         this.token = resp.data.clientToken;
-        //         console.log("Token ottenuto dalla chiamata a Braintree", this.token);
-        //     })
-        //     .finally(
-        //         this.brainTreeInit
-        //     )
+        this.importCart()
     }
 }
 
@@ -83,21 +117,35 @@ export default {
 
 <template>
     <button @click="generatePaymentForm">Genera form</button>
+    <div id="checkout-form" class="w-75 mx-auto">
+        <label for="interested_user_name">Nome</label>
+        <input class="w-100" required v-model="dataToSend.interested_user_name" id="interested_user_name" type="text">
+
+        <label for="interested_user_surname">Cognome</label>
+        <input class="w-100" required v-model="dataToSend.interested_user_surname" id="interested_user_surname" type="text">
+
+        <label for="interested_user_address">Indirizzo</label>
+        <input class="w-100" required v-model="dataToSend.interested_user_address" id="interested_user_address" type="text">
+
+        <label for="interested_user_email">Email</label>
+        <input class="w-100" v-model="dataToSend.interested_user_email" id="interested_user_email" type="email">
+
+        <label for="interested_user_phone">Telefono</label>
+        <input class="w-100" required v-model="dataToSend.interested_user_phone" id="interested_user_phone" type="text">
+
+    </div>
     <section class="ms_checkout-section">
-       <div id="dropin-wrapper">
+        <div id="dropin-wrapper">
             <div id="checkout-message"></div>
             <div id="dropin-container"></div>
             <button id="submit-button">Submit payment</button>
-        </div> 
+        </div>
     </section>
-    
 </template>
 
 <style lang="scss" scoped>
-
-    .ms_checkout-section {
-        min-height: 480px;
-        padding: 3rem;
-    }
-
+.ms_checkout-section {
+    min-height: 480px;
+    padding: 3rem;
+}
 </style>
